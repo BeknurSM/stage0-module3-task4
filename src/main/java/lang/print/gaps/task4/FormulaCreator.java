9d33a32f005483b1a8c58f7046cccf217e0ce81a
BekoSm <b.smagulov.1999@gmail.com>
2022-07-12 15:02:31 +0600

    Push#1

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,16 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        byte nine = 9;
+        char a = 'a';
+        char b = 'b';
+        byte two = 2;
+        byte five = 5;
+        byte seven = 7;
+        byte four = 4;
+        System.out.println((nine*a^2-5*b+2+a-7)*((a+b-4*a*b)/2));
+
+
+
     }
 }
